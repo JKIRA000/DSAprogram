f59f0c5c7cca217a507fc5fbe2b790bf80143916
John Joy <141158543+JKIRA000@users.noreply.github.com>
2023-11-10 22:36:05 +0530

    Update 46john25.c

--- 46john25.c
+++ 46john25.c
@@ -1,57 +1,38 @@
-include<stdio.h>
-#include <stdlib.h>
-
-void main(){
-
-    int choice,s,n;
-    printf("Enter the number of elements :");
-    scanf("%d",&n);
-    int a[n];
-    int beg,end,mid;
-
-    printf("Enter the elements of Array :\n");
-    for(int i=0;i<n;i++){
-        scanf("%d",&a[i]);
-    }
-
-    do{
-
-            printf("Press 1 to Search \nPress 2 to Exit :");
-            scanf("%d",&choice);
-
-        switch(choice){
-
-            case 1:
-
-                printf("Enter element to search : ");
-                scanf("%d",&s);
-
-                beg=0;
-                end=n-1;
-                mid=(beg+end)/2;
-
-                while((beg<end) && (a[mid]!=s)){
-                    if(s<a[mid]){
-                        beg=0;
-                        end=mid-1;
-                    }
-                    else{
-                        beg=mid+1;
-                        mid=(beg+end)/n;
-                    }
-                }
-                if(a[mid]==s){
-                    printf("\nElement Found \n");
-                }
-                else{
-                    printf("\nElement Not Found\n");
-                }
-
-            case 2:
-                exit(1);
-            default:
-                printf("Invalid Choice");
-
-        }
-    }while(1);
+/*
+Roll no:52
+Batch:C
+Author Name:Savin Johnson
+Date:17/10/23
+Description:Program for implementation of Binary search
+*/
+#include <stdio.h>
+int binaryScr(int a[], int low, int high, int m)
+{
+if (high >= low) {
+int mid = low + (high - low) / 2;
+if (a[mid] == m)
+return mid;
+if(a[mid] > m)
+return binaryScr(a, low, mid - 1, m);
+return binaryScr(a, mid + 1, high, m);
+}
+return -1;
+}
+int main(void)
+{
+int a[] = { 12, 13, 21, 36, 40 };
+int i,m;
+for(i=0;i<5;i++)
+{
+printf(" %d",a[i]);
+}
+printf(" n");
+int n = sizeof(a) / sizeof(a[0]);
+printf("Enter the number to be searched");
+scanf("%d", &m);
+int result = binaryScr(a, 0, n - 1, m);
+(result == -1) ? printf("The element is not present in array"):
+printf("The element is present at index %d",
+result);
+return 0;
 }
