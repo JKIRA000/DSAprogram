4e0e14c13fb1e1751f04caa96e4f3c68458cb223
John Joy <141158543+JKIRA000@users.noreply.github.com>
2023-11-10 22:09:06 +0530

    Update 46John1.c

--- 46John1.c
+++ 46John1.c
@@ -1,68 +1,38 @@
-
+/*
+Roll no:46
+Batch:B
+Author Name:John Joy
+Date:2/09/23
+Description:Determine if no is in Fibonacci sequence
+*/
 #include <stdio.h>
+  int main() {
+        int data, num1 = 0, num2 = 1, temp, flag = 0;
 
-#define MAX_SIZE 100
+        printf("Enter ur input:");
+        scanf("%d", &data);
 
-int main() {
-    int array[MAX_SIZE];
-    int size = 0;
-    int choice, element, position;
-
-    while (1) {
-        printf("\nMenu:\n");
-        printf("1. Insert Element\n");
-        printf("2. Display Array\n");
-        printf("3. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                if (size < MAX_SIZE) {
-                    printf("Enter the element : ");
-                    scanf("%d", &element);
-                    printf("Enter the position to insert (0 - %d): ", size);
-                    scanf("%d", &position);
-
-                    if (position < 0 || position > size) {
-                        printf("Invalid position Insertion process failed.\n");
-                    } else {
-                        // Shift elements to make space for the new element
-                        for (int i = size; i > position; i--) {
-                            array[i] = array[i - 1];
-                        }
-                        array[position] = element;
-                        size++;
-
-                        printf("Element insertion is successfull.\n");
-                    }
-                } else {
-                    printf("Array is full. Insertion failed.\n");
-                }
-                break;
-
-            case 2:
-                if (size > 0) {
-                    printf("Array elements: ");
-                    for (int i = 0; i < size; i++) {
-                        printf("%d ", array[i]);
-                    }
-                    printf("\n");
-                } else {
-                    printf("Array is empty.\n");
-                }
-                break;
-
-            case 3:
-                printf("Exiting program.\n");
+        if (data == num1 || data == num2) {
+                printf("%d is a fibonacci number\n", data);
                 return 0;
-
-            default:
-                printf("Invalid choice. Please select a valid option.\n");
         }
-    }
 
-    return 0;
-}
+        while (num2 <= data) {
+                temp = num2;
+                num2 = num1 + num2;
+                num1 = temp;
+                if (num2 == data) {
+                        flag = 1;
+                        break;
+                }
+        }
+
+        if (flag) {
+                printf("%d is a fibonacci number\n", data);
+        } else {
+                printf("%d is not a fibonacci number\n", data);
+        }
+        return 0;
+  }
 
 
