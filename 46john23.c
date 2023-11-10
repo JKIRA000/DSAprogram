a880b0e005f0185e87f93291e4279b8ff80505e9
John Joy <141158543+JKIRA000@users.noreply.github.com>
2023-11-10 22:34:35 +0530

    Update 46john23.c

--- 46john23.c
+++ 46john23.c
@@ -1,73 +1,71 @@
-include <stdio.h>
-
-// Recursive function to perform merge sort on arr[l..r]
-void msort(int arr[], int l, int r) {
-    if (l < r) {
-        
-        int m = l + (r - l) / 2;    // Middle point
-
-        // "Recursively sort"
-        msort(arr, l, m);               // [1,3,4,2] --> [1,3]  [4,2]  --> [1][3][4][2]
-        msort(arr, m + 1, r);
-
-
-        // "Merge" the sorted halves
-        int n1 = m - l + 1;
-        int n2 = r - m;
-        int L[n1], R[n2];   // Create temporary arrays
-
-
-        // "Copy data" to temporary arrays L[] and R[]
-        for (int i = 0; i < n1; i++)
-            L[i] = arr[l + i];
-        for (int j = 0; j < n2; j++)
-            R[j] = arr[m + 1 + j];
-
-        int i = 0, j = 0, k = l;
-
-
-        //--------------  Merge the temporary arrays -----------------------
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
-            } else {
-                arr[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-
-
-        // ---------------- Copy the remaining elements of L[] and R[] -----------------
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
-        }
+/*
+Roll no:46
+Batch:B
+Author Name:John Joy
+Date:2/10/23
+Description:Program to implement Merge Sort 
+*/
+#include<stdio.h>
+#include<stdlib.h>
+void partition(int a[], int low, int high);
+void main(){
+    int n,temp,i,j,a[20];
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+    printf("Enter Elements:");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("\nBefore Sorting:");
+    for(i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+    partition(a,0,n-1);
+    printf("\nAfter Sorting:");
+    for(i=0;i<n;i++){
+        printf("%d ",a[i]);
     }
 }
-
-int main() {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Unsorted array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+void partition(int a[], int low, int high){
+    int mid=0;
+    if(low<high){                   //divide the array into half till it has single element
+        mid=(low+high)/2;
+        partition(a,low,mid);
+        partition(a,mid+1,high);
+        mergesort(a,low,mid,high);
     }
-
-    msort(arr, 0, n - 1);
- 
-    printf("\nSorted array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+}
+mergesort(int a[],int low, int mid, int high){
+    int i,m,k,l,temp[20];
+    l=low;
+    i=low;                          //iterator for merged array
+    m=mid+1;
+    while(l<=mid && m<=high){       //this will iterate till all the element from left and right subarray are compared.
+        if(a[l]<=a[m]){             //a[l] points to first element of left sub array,a[m] points to first element of right sub array
+            temp[i]=a[l];           //temp is merged array
+            l++;
+            i++;
+        }
+        else{
+            temp[i]=a[m];
+            m++;
+            i++;
+            
+        }
+    }//end of while
+    if(l>mid){
+        for(k=m;k<=high;k++){
+            temp[i]=a[k];
+            i++;
+        }
     }
-
-    return 0;
+    else{
+        for(k=l;k<=mid;k++){
+            temp[i]=a[k];
+            i++;
+        }
+    }
+    for(k=low;k<=high;k++){
+        a[k]=temp[k];
+    }
 }
