3288c3fcf6374f29c36406c2a2562452533b529e
John Joy <141158543+JKIRA000@users.noreply.github.com>
2023-11-10 22:26:03 +0530

    Update 46john15.c

--- 46john15.c
+++ 46john15.c
@@ -1,3 +1,10 @@
+/*
+Roll no:46
+Batch:B
+Author Name:John Joy
+Date:2/09/23
+Description: Program to implement insert, delete, display, search in linked list
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
