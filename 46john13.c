d19656b2acde5ffe8b1f639cd5ab7c682b3ed282
John Joy <141158543+JKIRA000@users.noreply.github.com>
2023-11-10 22:23:30 +0530

    Update 46john13.c

--- 46john13.c
+++ 46john13.c
@@ -1,136 +1,90 @@
+/*
+Roll no:46
+Batch:B
+Author Name:John Joy
+Date:2/09/23
+Description: Array Implementation of Circular Queue
+*/
 #include<stdio.h>
 #include<stdlib.h>
-
-#define MAX_SIZE 20
-
-int queue[MAX_SIZE];
-int front = -1, rear = -1;
-int data;
-
-void EnQRear(int x){
-	
-	if ((rear == MAX_SIZE - 1 && front == 0) || (front == rear + 1)) {
-		printf("Queue is Overflow\n");
-	}
-	else{
-		if (front == -1 && rear == -1) {
-			front = rear = 0;
-	} else {
-	
-		rear =(rear+1)%MAX_SIZE;
-		queue[rear] = data;
-	    	printf("%d Enqueued successfully.\n", data);
-	    	
-	    }
-    }
-}
-
-void EnQFront(int x){
-		
-	if ((rear == MAX_SIZE - 1 && front == 0) || (front == rear + 1)) {
-	printf("Queue is Overflow\n");
-	}
-	else{
-	    if (front == 0) {
-		front = MAX_SIZE -1;
-		queue[front]= data;
-		printf("%d Enqueued successfully.\n", data);
-	    } else {
-		front--;
-		queue[front] = data;
-	        printf("%d Enqueued successfully.\n", data);
-	    }
-	}
-}
-
-void DeQFront() {
-    if (front == -1 && rear == -1) {
-        printf("Queue is Underflow\n");
-    }
-
-    printf("%d Dequeued successfully.\n", queue[front]);
-
-    if (front == rear) {
-        front = rear = -1;
-    } else {
-        front=(front+1)%MAX_SIZE;
-    }
-}
-
-void DeQRear(){
-
-	if (front == -1 && rear == -1) {
-        printf("Queue is Underflow\n");
-	}
-
-	printf("%d Dequeued successfully.\n", queue[front]);
-
-	if (front == rear) {
-		front = rear = -1;
-	} else {
-		
-		if( rear == 0 ){
-		
-			rear = MAX_SIZE -1;
-		
-		}
-		else{
-		
-			rear--;
-		
-		}
-		
-	}
-
-}
-
-void display() {
-    if (front == -1 && rear == -1) {
-        printf("Queue is Underflow\n");
-    }
-
-    printf("Queue elements: ");
-    for (int i = front; i <= rear; i++) {
-        printf("%d ", queue[i]);
-    }
-    printf("\n");
-}
-
-
-int main() {
-    int choice;
-
-    while (1) {
-        printf("\n\n1. Enqueue from rear\n2. Enqueue from front\n3. Dequeue from front\n4. Dequeue from rear\n5. Display\n6. Exit\n");
-        printf("\nEnter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                printf("\nEnter data to enqueue from rear: ");
-                scanf("%d", &data);
-                EnQRear(data);
-                break;
-            case 2:
-                printf("\nEnter data to enqueue from front: ");
-                scanf("%d", &data);
-                EnQFront(data);
-                break;
-            case 3:
-                DeQFront();
-                break;
-            case 4:
-                DeQRear();
-                break;
-            case 5:
-                display();
-                break;
-            case 6:
-                exit(0);
-            default:
-                printf("\nInvalid choice.\n");
+#define n 10
+void enqueue(int x);
+void dequeue();
+void display();
+int f=-1;
+int r=-1;
+int queue[n];
+void main(){
+    int ch,x;
+    while(1){
+        printf("\n----Queue Operations----\n");
+        printf("1. Enqueue Element\n");
+        printf("2. Dequeue Element\n");
+        printf("3. Display Elements\n");
+        printf("4. Exit\n");
+    printf("Enter Number:\n");
+    scanf("%d",&ch);
+    switch(ch){
+        case 1:
+            printf("\nEnter Element to be Inserted: ");
+            scanf("%d",&x);
+            enqueue(x);
+            break;
+        case 2:
+            dequeue();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(1);
+            break;
+        default:
+            printf("Invalid Number.\n");
         }
     }
-
-    return 0;
+}
+void enqueue(int x){
+    if((f==0 && r==n-1) || f==r+1){
+        printf("Queue is Full.\n");
+    }
+    else{
+        if(f==-1 && r== -1){
+            f++;
+            r++;
+            queue[r]=x;
+            printf("Element %d has been inserted.\n",x);
+        }
+        else{
+            r=(r+1)%n;
+            queue[r]=x;
+            printf("Element %d has been inserted.\n",x);
+        }
+    }
+}
+void dequeue(){
+    if(f==-1 && r== -1){
+        printf("Queue is Empty.");
+    }
+    else{
+        if(f==r){
+            f=-1;
+            r=-1;
+        }
+        else{
+            f=(f+1)%n;
+            printf("Element has been deleted.\n");
+        }
+    }
+}
+void display(){
+    if(f==-1 && r== -1){
+        printf("Queue is Empty.");
+    }
+    else{
+        printf("Elements are:\n");
+        for(int i=f;i<=r;i++){
+            printf("%d\n",queue[i]);
+        }
+    }
 }
