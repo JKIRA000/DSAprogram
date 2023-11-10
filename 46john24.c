c4f8fe9776305708b6e6653d5d8eca0de439cc90
John Joy <141158543+JKIRA000@users.noreply.github.com>
2023-11-10 22:35:42 +0530

    Update 46john24.c

--- 46john24.c
+++ 46john24.c
@@ -1,60 +1,68 @@
-#include<stdio.h>
+/*
+Roll no:46
+Batch:B
+Author Name:John Joy
+Date:2/10/23
+Description:Program to implement Quick Sort 
+*/
+#include <stdio.h>
 
-void swap(int *a,int *b){
-    int temp=*a;
-    *a=*b;
-    *b=temp;
+// Function to swap two elements in an array
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
 
-void qsort(int a[], int l, int h) {
+// Function to partition the array into two subarrays, elements less than the pivot
+// and elements greater than or equal to the pivot
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; // Choose the rightmost element as the pivot
+    int i = (low - 1);    // Index of the smaller element
 
-    if (l < h) {
-        
-        int target = a[l]; // Choose the 1st element as the target
-        int i = l;  
-
-        for (int j = l + 1; j <= h; j++) {
-            
-            // If Target is greater than current element
-            if (target >= a[j]) {
-                
-                i++;                // Increment index of the smaller element
-                swap(&a[i], &a[j]); // Swap a[i] and a[j]
-            }
+    for (int j = low; j <= high - 1; j++) {
+        // If the current element is smaller than or equal to the pivot
+        if (arr[j] <= pivot) {
+            i++; // Increment index of the smaller element
+            swap(&arr[i], &arr[j]);
         }
+    }
+    swap(&arr[i + 1], &arr[high]); // Place the pivot element in its correct position
+    return (i + 1);
+}
 
-        swap(&a[i], &a[l]);
+// Function to perform Quick Sort
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        // Partition the array into two subarrays
+        int pi = partition(arr, low, high);
 
-        // Recursively sort elements
-        qsort(a, l, i - 1);
-        qsort(a, i + 1, h);
+        // Recursively sort the subarrays
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
 }
 
-int main(){
-
+int main() {
     int n;
-    printf("\n Enter the numbers to be sorted :");
-    scanf("%d",&n);
-    int a[n];
 
-    printf("\n Enter Numbers one by one : \n");
-    for(int i=0;i<n;i++){
-        scanf("%d",&a[i]);
-    }
-    printf("\n Before Sorting : \n");
-    for(int i=0;i<n;i++){
-        printf("%d ",a[i]);
-    }
-    
-    // -------------- Sorting Starts ----------------
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
 
-    qsort(a,0,n-1);
+    int arr[n];
 
-    // -------------- Sorting Ends ------------------
-    printf("\n After Sorting : \n");
-    for(int i=0;i<n;i++){
-        printf("%d ",a[i]);
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-   return 0; 
+
+    quickSort(arr, 0, n - 1); // Call quickSort to sort the array
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
