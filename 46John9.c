ce117e11bf20bfdd05458044b3884bd311018b2e
John Joy <141158543+JKIRA000@users.noreply.github.com>
2023-11-10 22:18:12 +0530

    Update 46John9.c

--- 46John9.c
+++ 46John9.c
@@ -1,3 +1,10 @@
+/*
+Roll no:46
+Batch:B
+Author Name:John Joy
+Date:2/09/23
+Description:print duplicate elements
+*/
 #include <stdio.h>
 
 int main() {
@@ -41,4 +48,3 @@ int main() {
 
     return 0;
 }
-
